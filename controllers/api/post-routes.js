a7cc92beb8a3c6b10511384236b6093ad11d28a3
Deiontre10 <travontazlowry@gmail.com>
2023-01-22 18:25:29 -0500

    started on post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,34 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
+const { Post, User, Comment } = require('../../models');
+const withAuth = require('../utils/auth');
 
+router.get('/', async (req, res) => {
+    try {
+        const data = await Post.findAll({
+            attributes: ["id", "title", "body", "date_made"],
+            order: [['date_made', 'DESC']],
+            include: [
+                {
+                    model: User,
+                    attributes: ["username"]
+                },
+                {
+                    model: Comment,
+                    attributes: ["id", "body", "post_id", "user_id", "date_made"],
+                    include: [{
+                        model: User,
+                        attributes: ["username"]
+                    }]
+                },
+            ]
+        });
+
+        res.status(200).json(data)
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 module.exports = router
