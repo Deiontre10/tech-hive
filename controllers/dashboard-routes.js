9fb5275df20dc80b23082e695199d9f0e0bb0934
Deiontre10 <travontazlowry@gmail.com>
2023-01-24 12:21:41 -0500

    updated dashboard-routes.js

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -35,17 +35,74 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
-router.post('/new'), withAuth, async (req, res) => {
+router.get('/edit/:id', withAuth, async (req, res) => {
   try {
-    const data = await Post.create({
-      title: req.body.title,
-      content: req.body.content,
-      date_made: req.body.date_made
+    const editPost = await Post.findOne({
+      where: {
+        user_id: req.params.user_id
+      },
+      attributes: ['id', 'title', 'content', 'date_made'],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'body', 'post_id', 'user_id', 'date_made'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
     });
-    res.status(200).json(data);
+
+    const posts = editPost.get({ plain: true });
+
+    res.render('edit', {
+      posts,
+      loggedIn: req.session.loggedIn
+    })
+
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-}
+});
+
+router.get('/new', withAuth, async (req, res) => {
+  try {
+    const makePost = await Post.findAll({
+      where: {
+        user_id: req.session.user_id
+      },
+      attributes: ['id', 'title', 'content', 'date_made'],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'body', 'post_id', 'user_id', 'date_made'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+
+    const posts = makePost.map(post => post.get({ plain: true }));
+
+    res.render('create', {
+      posts,
+      loggedIn: req.session.loggedIn
+    })
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
 
 module.exports = router;
