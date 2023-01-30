ce472be94d21e71ab4b7fbe574642df6777dc4ae
Deiontre10 <travontazlowry@gmail.com>
2023-01-30 15:49:54 -0500

    setting up views and js files

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -38,9 +38,6 @@ router.get('/', withAuth, async (req, res) => {
 router.get('/edit/:id', withAuth, async (req, res) => {
   try {
     const editPost = await Post.findOne({
-      where: {
-        user_id: req.params.user_id
-      },
       attributes: ['id', 'title', 'content', 'date_made'],
       include: [
         {
@@ -70,39 +67,36 @@ router.get('/edit/:id', withAuth, async (req, res) => {
   }
 });
 
-router.get('/new', withAuth, async (req, res) => {
-  try {
-    const makePost = await Post.findAll({
-      where: {
-        user_id: req.session.user_id
-      },
-      attributes: ['id', 'title', 'content', 'date_made'],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'body', 'post_id', 'user_id', 'date_made'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
-    })
+router.get('/new', withAuth, (req, res) => {
+  // try {
+  //   const makePost = await Post.findAll({
+  //     attributes: ['id', 'title', 'content', 'date_made'],
+  //     include: [
+  //       {
+  //         model: Comment,
+  //         attributes: ['id', 'body', 'post_id', 'user_id', 'date_made'],
+  //         include: {
+  //           model: User,
+  //           attributes: ['username']
+  //         }
+  //       },
+  //       {
+  //         model: User,
+  //         attributes: ['username']
+  //       }
+  //     ]
+  //   })
 
-    const posts = makePost.map(post => post.get({ plain: true }));
+    // const posts = makePost.map(post => post.get({ plain: true }));
 
     res.render('create', {
-      posts,
+      // posts,
       loggedIn: req.session.loggedIn
     })
 
-  } catch (err) {
-    res.status(500).json(err)
-  }
+  // } catch (err) {
+  //   res.status(500).json(err)
+  // }
 });
 
 module.exports = router;
